6f761053fb542f98f136330fe50f9fa07419a55b
shayanfpg9 <shayanfpg9@duck.com>
2024-10-09 20:10:10 +0330

    config settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,23 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
   pluginJs.configs.recommended,
 ];
